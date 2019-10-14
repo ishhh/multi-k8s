6a267b7a5c9796f7c87ca4a1e30efde379f3deff
silkin.i <silkin.i@volgablob.ru>
2019-10-14 20:45:44 +0400

    restore app

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,10 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+
 class App extends Component {
   render() {
     return (
@@ -18,3 +25,5 @@ class App extends Component {
     );
   }
 }
+
+export default App;
